ddba4df8614f23a6b8b6c944b7c65eabd2784efd
yangdaqiong <yangdaqiong@126.com>
2024-08-20 01:37:36 +0800

    project init

---
+++ pages/admin/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>后台首页</div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin'
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-red-700">Hello world!</h1>
+  </div>
+</template>
